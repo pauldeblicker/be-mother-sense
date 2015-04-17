e6973d6099d2774c9fff49172fb4e2e5f8fc1d16
Pouli <paul.deblicker@isen-lille.fr>
2015-04-17 11:33:57 +0200

    routes sendStatus modification

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,17 @@ router.get('/', function(req, res, next) {
 
 router.post('/mother/temperatures', function(req, res) {
   console.log(req.body);
+  res.sendStatus(200);
 });
 
 router.post('/mother/motions', function(req, res) {
   console.log(req.body);
+  res.sendStatus(200);
 });
 
 router.post('/mother/batteries', function(req, res) {
   console.log(req.body);
+  res.sendStatus(200);
 })
 
 module.exports = router;
