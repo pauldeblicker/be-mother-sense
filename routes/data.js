f41e58de63d3c7e60a21d1a8ff081137ac29c4c0
Pouli <paul.deblicker@isen-lille.fr>
2015-04-20 10:55:36 +0200

    Query date modification

--- routes/data.js
+++ routes/data.js
@@ -6,9 +6,10 @@ var Motion = require('./models/motion');
 
 /* GET users listing. */
 router.get('/temperature/:cookie', function(req, res, next) {
-  Temperature.find({'cookie': req.params.cookie})
+  var date = Date.now();
+  date.setDate(date.getDate()-1);
+  Temperature.find({'cookie': req.params.cookie, 'date': {$gte: date}})
     .sort({'date': -1})
-    .limit(96)
     .exec(function(err, data) {
       if(err) {
         res.json(err);
@@ -21,7 +22,19 @@ router.get('/temperature/:cookie', function(req, res, next) {
 });
 
 router.get('/motion/:cookie', function(req, res, next) {
-
+  var date = Date.now();
+  date.setDate(date.getDate()-1);
+  Motion.find({'cookie': req.params.cookie, 'date': {$gte: date}})
+    .sort({'date': -1})
+    .exec(function(err, data) {
+      if(err) {
+        res.json(err);
+      } else if(!data) {
+        res.json({success: false});
+      } else {
+        res.json({success: true, data: data});
+      }
+    });
 });
 
 module.exports = router;
